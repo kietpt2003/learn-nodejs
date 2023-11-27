f06c9e0fc1377962f9aed6046c077aefae372800
kietpt2003 <tuankiet29012003@gmail.com>
2023-11-27 17:04:14 +0700

    using template engine ejs

--- src/server.js
+++ src/server.js
@@ -1,22 +1,29 @@
-const mysql = require('mysql2')
-const express = require('express')
+const mysql = require('mysql2');
+const path = require('path');
+const express = require('express');
 const app = express();
-const port = 8081
+const port = 8081;
+
+
+app.set('views', path.join(__dirname, 'viewsfolder'));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('Hello World');
 })
 
 app.get('/test', function (req, res) {
-    res.send('test route')
+    res.send('test route');
 })
 
 app.get('/kiet', function (req, res) {
-    res.send('<h1>test kiet route</h1>')
+    res.render('sample.ejs');
 })
 
+
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 })
 
 // create the connection to database
