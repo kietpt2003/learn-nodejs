f4e3e8013efeea88e6fbcbe7e3f1511df8be229b
kietpt2003 <tuankiet29012003@gmail.com>
2023-11-27 17:13:31 +0700

    using .env

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql2');
 const path = require('path');
+const env = require('dotenv');
+env.config();
+console.log('check .env', process.env);
 const express = require('express');
 const app = express();
-const port = 8081;
-
+const port = process.env.PORT;
 
 app.set('views', path.join(__dirname, 'viewsfolder'));
 app.set('view engine', 'ejs');
@@ -28,11 +30,11 @@ app.listen(port, () => {
 
 // create the connection to database
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307, //default 3306
-    user: 'root',
-    password: '123456', //default empty
-    database: 'nodejsdb'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, //default 3306
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD, //default empty
+    database: process.env.DB_NAME
 });
 
 // simple query
